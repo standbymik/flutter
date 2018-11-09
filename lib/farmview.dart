ec2801b962b6921bae45ea2d7ebf52b8c79854d2
StandbyMik <standbypm4ever@gmail.com>
2018-11-09 15:39:36 +0700

    farms1

--- lib/farmview.dart
+++ lib/farmview.dart
@@ -1,15 +1,40 @@
-class Farmview {
-  final String image;
-  final String farm;
-  final String body;
+import 'dart:convert';
 
-  Farmview({this.image,this.farm, this.body});
+import 'package:flutter/foundation.dart';
 
-  factory Farmview.fromJson(Map<String, dynamic> json) {
-    return Farmview(
-      image:json['image'],
-      farm:json['name'],
-      body: json['body'],
+import 'package:http/http.dart' as http;
+
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  final response =
+      await client.get('https://jsonplaceholder.typicode.com/photos');
+
+  // Use the compute function to run parsePhotos in a separate isolate
+  return compute(parsePhotos, response.body);
+}
+
+// A function that will convert a response body into a List<Photo>
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  Photo({this.albumId, this.id, this.title, this.url, this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      albumId: json['albumId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      url: json['url'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,69 @@
+import 'dart:async';
 import 'dart:convert';
-
+import 'farmview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'farmview.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final appTitle = 'Isolate Demo';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-          appBar: AppBar(title: Text('FarmStay.asia')), body: MyHomePage()),
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-final Future<Farmview> farms;
-  MyHomePage({Key key, this.farms}) : super(key: key);
+  final String title;
 
-  Future<Farmview> fetchPost() async {
-    final response =
-        await http.get('https://www.friendflock.com/Farmstay/test.php?action=farms');
-    if (response.statusCode == 200) {
-      // If server returns an OK response, parse the JSON
-      return Farmview.fromJson(json.decode(response.body));
-    } else {
-      // If that response was not OK, throw an error.
-      throw Exception('Failed to load post');
-    }
-  }
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: FutureBuilder<Farmview>(
-            future: farms,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.image);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
 
-              // By default, show a loading spinner
-              return CircularProgressIndicator();
-            },
-          ),
+          return snapshot.hasData
+              ? PhotosList(photos: snapshot.data)
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
     );
   }
 }
 
+class PhotosList extends StatelessWidget {
+  final List<Photo> photos;
 
-class Farmview {
-  final String image;
-  final String farm;
-  final String body;
+  PhotosList({Key key, this.photos}) : super(key: key);
 
-  Farmview({this.image,this.farm, this.body});
-
-  factory Farmview.fromJson(Map<String, dynamic> json) {
-    return Farmview(
-      image:json['image'],
-      farm:json['name'],
-      body: json['body'],
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Column(
+          children: <Widget>[
+            Text(photos[index].title),
+            Image.network(photos[index].thumbnailUrl),
+          ],
+        );
+      },
     );
   }
 }
